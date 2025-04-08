8851c480db32f976833a825087296b566fd675d0
mohini chauhan 22SCSE2030513 <you@example.com>
2025-04-08 20:13:30 +0530

    initial commit

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector(".navbar-nav");
+    menu.classList.toggle("open")
+    
+  }
+  function show(){
+    window.location.href="index.html"
+  }
